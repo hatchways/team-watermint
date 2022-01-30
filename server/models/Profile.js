59b1bdd9dcb529aac70841fd066d7c6c7054238a
Gillpreet <gillpreetsinghgill@gmail.com>
2022-01-30 00:56:33 -0800

    iss-43 Added profile type enum

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -4,7 +4,12 @@ const profileSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
-    ref: 'User'
+    ref: 'User',
+  },
+  accountType: {
+    type: String,
+    default: "",
+    enum: ['pet_owner', 'pet_sitter'],
   },
   name: {
     type: String,
