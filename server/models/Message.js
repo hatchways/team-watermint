9ca38e859efea6c0b4bfbe067c3e7205b9292809
Edward Olmos <edwardolmos3@gmail.com>
2022-02-09 08:57:19 -0800

    created Conversation and Message models

---
+++ server/models/Message.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  conversationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Conversation',
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  }
+}, { timestamps: true });
+
+module.exports = Message = mongoose.model("Message", messageSchema);
