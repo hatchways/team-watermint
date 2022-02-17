ffc873e7b8d2f4305494b68c2d5d535c6951dad5
denjj <25715300+denjj@users.noreply.github.com>
2022-02-17 07:32:49 -0800

    Add loading spinner for protected route

--- client/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,3 +1,4 @@
+import { CircularProgress, Stack } from '@mui/material';
 import { Route, Redirect, RouteProps } from 'react-router-dom';
 import { useAuth } from '../../context/useAuthContext';
 
@@ -5,7 +6,13 @@ export default function ProtectedRoute({ ...props }: RouteProps): JSX.Element {
   const { loggedInUser } = useAuth();
   const redirectPath = '/login';
 
-  if (loggedInUser) {
+  if (typeof loggedInUser === 'undefined')
+    return (
+      <Stack alignItems="center" marginTop={'20vh'}>
+        <CircularProgress />
+      </Stack>
+    );
+  else if (loggedInUser) {
     return <Route {...props} />;
   } else {
     return (
