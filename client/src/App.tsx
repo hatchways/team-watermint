8c571a5fa17416c8147574f5729313bb83906585
denjj <25715300+denjj@users.noreply.github.com>
2022-03-02 14:24:35 -0800

    Move localization provider from calendar to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './themes/theme';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { LocalizationProvider } from '@mui/lab';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import Login from './pages/Login/Login';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Signup from './pages/SignUp/SignUp';
@@ -20,25 +22,27 @@ function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <SnackBarProvider>
-          <AuthProvider>
-            <SocketProvider>
-              <CssBaseline />
-              <Navbar />
-              <Switch>
-                <Route exact path="/" component={LandingPage} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/signup" component={Signup} />
-                <Route exact path="/dashboard" component={Dashboard} />
-                <Route path="/my-jobs" component={Bookings} />
-                <Route path="/profile/settings" component={Settings} />
-                <Route path="*">
-                  <NotFound />
-                </Route>
-              </Switch>
-            </SocketProvider>
-          </AuthProvider>
-        </SnackBarProvider>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <SnackBarProvider>
+            <AuthProvider>
+              <SocketProvider>
+                <CssBaseline />
+                <Navbar />
+                <Switch>
+                  <Route exact path="/" component={LandingPage} />
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/signup" component={Signup} />
+                  <Route exact path="/dashboard" component={Dashboard} />
+                  <Route path="/my-jobs" component={Bookings} />
+                  <Route path="/profile/settings" component={Settings} />
+                  <Route path="*">
+                    <NotFound />
+                  </Route>
+                </Switch>
+              </SocketProvider>
+            </AuthProvider>
+          </SnackBarProvider>
+        </LocalizationProvider>
       </BrowserRouter>
     </ThemeProvider>
   );
--- client/src/pages/Bookings/Calendar/Calendar.tsx
+++ client/src/pages/Bookings/Calendar/Calendar.tsx
@@ -1,5 +1,4 @@
-import { LocalizationProvider, PickersDay, StaticDatePicker } from '@mui/lab';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import { PickersDay, StaticDatePicker } from '@mui/lab';
 import { TextField } from '@mui/material';
 import { Request } from '../../../interface/RequestApiData';
 
@@ -12,23 +11,21 @@ export default function Calendar({ requests }: Props): JSX.Element {
   const highlightedDays = requests.map((x) => new Date(x.start).toDateString());
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <StaticDatePicker
-        displayStaticWrapperAs="desktop"
-        value={initialDate}
-        readOnly={true}
-        views={['day']}
-        showDaysOutsideCurrentMonth={true}
-        onChange={() => {
-          null;
-        }}
-        renderInput={(params) => <TextField {...params} />}
-        renderDay={(day, _value, DayComponentProps) => {
-          if (!day) return <PickersDay {...DayComponentProps} selected={false} />;
-          const isSelected = !DayComponentProps.outsideCurrentMonth && highlightedDays.includes(day.toDateString());
-          return <PickersDay {...DayComponentProps} selected={isSelected ? true : false} />;
-        }}
-      />
-    </LocalizationProvider>
+    <StaticDatePicker
+      displayStaticWrapperAs="desktop"
+      value={initialDate}
+      readOnly={true}
+      views={['day']}
+      showDaysOutsideCurrentMonth={true}
+      onChange={() => {
+        null;
+      }}
+      renderInput={(params) => <TextField {...params} />}
+      renderDay={(day, _value, DayComponentProps) => {
+        if (!day) return <PickersDay {...DayComponentProps} selected={false} />;
+        const isSelected = !DayComponentProps.outsideCurrentMonth && highlightedDays.includes(day.toDateString());
+        return <PickersDay {...DayComponentProps} selected={isSelected ? true : false} />;
+      }}
+    />
   );
 }
